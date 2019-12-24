832541f0cd771a3d5f89910e9acbbb3bb77a0fae
Glitch (hello-webpage) <none>
2019-12-24 05:21:43 +0000

    🐩💒 Checkpoint ./server.py:9671916/184

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template, jsonify, Response
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello World!"
 
-@app.route("/listen")
+@app.route("/listen", methods=['POST'])
 def listen():
-  return "listening"
+  data = request.get_json(force=True)
+  return data
 
 if __name__ == "__main__":
   app.run()
