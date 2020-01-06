4cf5dd65705f6ffabfda7ec6a49ecdbc1a4ba799
Glitch (buttery-waxflower) <none>
2020-01-06 22:09:00 +0000

    🐟👭 Checkpoint ./bots/__init__.py:9665611/1594 ./server.py:9665611/811

--- bots/__init__.py
+++ bots/__init__.py
@@ -1,9 +1,49 @@
-import os
-import glob
+from bots.scrambler import Scrambler
+from bots.nice import Nice
+from bots.dm import Dm
+from bots.mock import Mock
+from bots.underscore import Underscore
+from bots.stabby import Stabby
+from bots.fire import Fire
+from bots.hello import Hello
+from bots.summon import Summon
+from bots.pig import Pig
+from bots.space_replace import SpaceReplace
+from bots.reminder import Reminder
+from bots.dnd import Dnd
+from bots.say_again import SayAgain
+from bots.new_ban import NewBan
+from bots.group_call import GroupCall
+from bots.censor import Censor
+from bots.deja_vu import DejaVu
+from bots.botify import Botify
+from bots.chaos_seed import ChaosSeed
+from bots.sandbox import Sandbox
+from bots.spam import Spam
+from bots.lol import Lol
 
-all_list = list()
-for f in glob.glob(os.path.dirname(__file__)+"/*.py"):
-    if os.path.isfile(f) and not os.path.basename(f).startswith('_'):
-        all_list.append(os.path.basename(f)[:-3])
-
-__all__ = all_list 
+__all__ = [
+  "Dm", 
+  "Mock", 
+  "Nice", 
+  "Underscore",
+  "Stabby",
+  "Fire",
+  "Hello",
+  "Summon",
+  "Pig",
+  "Scrambler",
+  "SpaceReplace",
+  "Reminder",
+  "Dnd",
+  "SayAgain",
+  "NewBan",
+  "GroupCall",
+  "Censor",
+  "DejaVu",
+  "Botify",
+  "ChaosSeed",
+  "Sandbox", 
+  "Spam",
+  "Lol"
+]
