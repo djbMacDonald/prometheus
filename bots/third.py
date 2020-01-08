3b45b30e7667ece8d221bedd8a4d226f745f06ea
Glitch (buttery-waxflower) <none>
2020-01-08 21:51:06 +0000

    👦🚝 Checkpoint ./bots/third.py:2978185/1150:9665611/87

--- bots/third.py
+++ bots/third.py
@@ -10,15 +10,27 @@ class Third(Bot):
   
   _target = "CJ"
   _frequency = 1
+  _post_text =""
   
   @classmethod
   def description(cls):
     return "`Third` Makes {} talk in third person".format(cls._target)
+  def contains_first(self):
+    container=False
+    if " i " in self._event.text().lower():
+      container=True
+    if "i " in self._event.text().lower():
+      container=True
+    if "i'm" in self._event.text().lower():
+      container=True
+    return container
   
   def run(self):
-    if self._event.isAMessage() and self._event.isFrom(self._target) and self._randomUtil.rollDice(self._frequency) and "i" in self._event.text().lower():
+    if self._event.isAMessage() and self._event.isFrom(self._target) and self._randomUtil.rollDice(self._frequency) and self.contains_first():
       identity = IDENTITIES[self._event.user()]
-      
-      newText = self._event.text().replace(" I ",self._target)
-      newText = newText.replace("I ",self._target)
-      self._postUtil.addMessage(newText, self._event.channel(), self._event.threadId(), Identity(identity.get('username'), identity.get('profilePicture')))
+      self._post_text=self._event.text().lower()
+      self._post_text = self._post_text.lower().replace(" i ",self._target+" ")
+      self._post_text = self._post_text.lower().replace("i ",self._target+" ")
+      self._post_text = self._post_text.lower().replace("i'm",self._target+" is")
+      self._postUtil.deleteMessage(self._event.channel(), self._event.id())
+      self._postUtil.addMessage(self._post_text, self._event.channel(), self._event.threadId(), Identity(identity.get('username'), identity.get('profilePicture')))
