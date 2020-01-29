5c6b86aa90a0a94f250833fe463ea4730d270ac7
Glitch (buttery-waxflower) <none>
2020-01-29 18:35:13 +0000

    ⛱☃️ Checkpoint ./bots/dial.py:9665611/60

--- bots/dial.py
+++ bots/dial.py
@@ -6,7 +6,7 @@ from constant.channels import CHANNELS
 class Dial(Bot):
   
   def run(self):
-    if self._event.channel() != 'CSZFZKGFK':
+    if self._event.channel() != 'CSZFZKGFK' or self._event.isFromABot() or not self._event.isAMessage():
       return
     identity = Identity(emoji = 'musical_note');
     self._postUtil.addMessageToChannel(self._event.text(), CHANNELS['Chaos'], identity)
