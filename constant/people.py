6ec399e3e83e055257394952b58dc8baad345336
Glitch (buttery-waxflower) <none>
2020-02-21 17:12:25 +0000

    🏐🏦 Checkpoint ./constant/people.py:2136290/3844

--- constant/people.py
+++ constant/people.py
@@ -52,3 +52,25 @@ IDENTITIES =  {
   'UK7V13DS7': {'username': 'May', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UK7V13DS7-1e2ae9f73e21-48', 'id': 'UK7V13DS7'},
   'UJTEHPEPL': {'username': 'Rick', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'}
 }
+
+PEOPLE = {
+  'UETS9HTSS': {'username': 'Brenden', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UETS9HTSS-2a4b268e8aca-48', 'id': 'UETS9HTSS'},
+  'UDDE5960N': {'username': 'CJ', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UDDE5960N-9141f5699aec-48', 'id': 'UDDE5960N'},
+  'UDC4QL95G': {'username': 'Dakota', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UDC4QL95G-11730d6b9fe2-48', 'id': 'UDC4QL95G'},
+  'UDBGH9BMX': {'username': 'Dave', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UDBGH9BMX-19a9cf1e0f26-48', 'id': 'UDBGH9BMX'},
+  'UDC03KCGK': {'username': 'Drewid', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UDC03KCGK-1df95781be09-48', 'id': 'UDC03KCGK'},
+  'UHQD9J1SL': {'username': 'Asyhu', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UHQD9J1SL-303a26778808-48', 'id': 'UHQD9J1SL'},
+  'UDD45P5A9': {'username': 'Victoria', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UDD45P5A9-5bb81e7c49c0-48', 'id': 'UDD45P5A9'},
+  'UK7V13DS7': {'username': 'Mei', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UK7V13DS7-1e2ae9f73e21-48', 'id': 'UK7V13DS7'},
+  'UJTEHPEPL': {'username': 'Richard', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'},
+  'UDL020K8D': {'username': 'Doug', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'},
+  'UPGVCMGD9': {'username': 'Marissa', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'},
+  'UJTEHPEPL': {'username': 'Richard', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'},
+  'UJTEHPEPL': {'username': 'Richard', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'},
+  'UJTEHPEPL': {'username': 'Richard', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'},
+  'UJTEHPEPL': {'username': 'Richard', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'},
+  'UJTEHPEPL': {'username': 'Richard', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'},
+  'UJTEHPEPL': {'username': 'Richard', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'},
+  'UJTEHPEPL': {'username': 'Richard', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'}
+
+}
