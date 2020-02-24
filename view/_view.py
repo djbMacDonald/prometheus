5a498a37aa07a064dc3b3cccf28b41c02d51bbee
Glitch (buttery-waxflower) <none>
2020-02-24 22:39:35 +0000

    📡🎤 Checkpoint ./view/_view.py:2136290/251

--- view/_view.py
+++ view/_view.py
@@ -1,15 +1,14 @@
 class View:
   
-  def __init__(self, request=None, user=None):
-    if request:
-      self.user = request.get('user_id')
-    else:
-      self.user = user
+  def __init__(self, user=None, view=None, state=None):
+    self.user = user.user
     self.external_id = None
     self.private_metadata = None
     self._blocks = []
     self._header = {"type": "modal"}
     self.setTitle('Sample Modal')
+    self.view = view
+    self.state =
 
     
     
@@ -22,8 +21,8 @@ class View:
     
   def _finalize(self):
     self.view = self._header
-    if self.external_id:
-      self.view['external_id'] = f"{self.external_id}_{self.user}"
+    if self.external_id and self.user.id:
+      self.view['external_id'] = f"{self.external_id}_{self.user.id}"
     if self.private_metadata:
       self.view['private_metadata'] = self.private_metadata
     self.view['blocks'] = self._blocks
