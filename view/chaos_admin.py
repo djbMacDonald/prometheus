4bb8678acbd2767dec75d6807337c82b5fdf8f25
Glitch (buttery-waxflower) <none>
2020-02-25 17:51:19 +0000

    🚥🍲 Checkpoint ./view/chaos_admin.py:2136290/1994

--- view/chaos_admin.py
+++ view/chaos_admin.py
@@ -3,47 +3,43 @@ from view._view import View
 class ChaosAdminView(View):
   
   def __init__(self, user):
-    super().__init__(user)    
+    super().__init__(user)
+    self.external_id = 'ChaosAdmin'
   
     
   def build(self):
     self.setTitle('Chaos Admin')
-    threadUsersCursor = user._db.users.find({'threads': True})
+    self.buildThreadUsers()
+    self._finalize()
+    return self.view
+  
+ 
+  def buildThreadUsers(self):
+    threadUsersCursor = self.user._db.users.find({'threads': True})
     options = []
     for threadUser in threadUsersCursor:
+      name = threadUser.get('profile').get('real_name')
       options.append(
         {
           "text": {
             "type": "plain_text",
-            "text": threadUser.get('profile').get('display_name'),
+            "text": name,
             "emoji": True
           },
-          "value": "wizarding",
-          "description": {
-            "type": "plain_text",
-            "text": "I would like to participate in magical duals",
-            "emoji": True
-          }
+          "value": threadUser.get('id')
         }
       )
                                      
     element = {
       "type": "checkboxes",
-      "options": self.getAllOptions(),
+      "options": options
     }
-    if selectedOptions:
-      element['initial_options'] = selectedOptions
+  
     self._blocks.append(
       {
         "type": "actions",
         "elements": [element]
       }
     )
-    return
     
-    
-    self._finalize()
-    return self.view
-  
- 
 
