27faed02f832ebaba24d3eb0c653101b7e261929
Glitch (buttery-waxflower) <none>
2020-03-04 03:31:57 +0000

    🚛❄️ Checkpoint ./constant/people.py:9665611/16

--- constant/people.py
+++ constant/people.py
@@ -47,7 +47,7 @@ IDENTITIES =  {
   'UDC4QL95G': {'username': 'Duhcoatah Quelley', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UDC4QL95G-11730d6b9fe2-48', 'id': 'UDC4QL95G'},
   #'UDBGH9BMX': {'username': 'Deeve Friedman', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UDBGH9BMX-19a9cf1e0f26-48', 'id': 'UDBGH9BMX'},
   'UDC03KCGK': {'username': 'Drow Carrington', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UDC03KCGK-1df95781be09-48', 'id': 'UDC03KCGK'},
-  'UHQD9J1SL': {'username': 'Ash Ketchum', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UHQD9J1SL-303a26778808-48', 'id': 'UHQD9J1SL'},
+  'UHQD9J1SL': {'username': 'Ayshy', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UHQD9J1SL-303a26778808-48', 'id': 'UHQD9J1SL'},
   'UDD45P5A9': {'username': 'Vee', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UDD45P5A9-5bb81e7c49c0-48', 'id': 'UDD45P5A9'},
   'UK7V13DS7': {'username': 'May', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UK7V13DS7-1e2ae9f73e21-48', 'id': 'UK7V13DS7'},
   'UJTEHPEPL': {'username': 'Rick', 'profilePicture': 'https://ca.slack-edge.com/TDBEDSEQZ-UJTEHPEPL-g426b093d697-48', 'id': 'UJTEHPEPL'}
