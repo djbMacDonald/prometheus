af21663161e29a8fdd840682cb94115cff204a88
Glitch (buttery-waxflower) <none>
2020-03-04 18:52:43 +0000

    🕷🎥 Checkpoint ./bots/birthday:2978185/1104 ./bots/birthday.py:2978185/580

---
+++ bots/birthday.py
@@ -0,0 +1,21 @@
+from constant.people import USERS
+from model.identity import Identity
+from bots._bot import Bot
+
+class Birthday(Bot):
+  
+  _frequency = 1
+  _target = 'Brenden'
+  
+  @classmethod
+  def description(cls):
+    return "`Birthday` Has a {}% chance to say happy brithday on {}'s' posts".format(cls._frequency * 100, cls._target)
+    
+  def run(self):
+    if not self._event.isFrom(self._target) or self._event.isPartOfAThread() or self._event.isFromABot():
+      return
+    if not self._randomUtil.rollDice(self._frequency):
+      return
+    message='Happy Birthday {}'.format(self._target)
+    
+    self._postUtil.addMessageToThread(message, self._event.channel(), self._event.id(), Identity(profilePicture = self._identityUtil.randomImageUrl()))
