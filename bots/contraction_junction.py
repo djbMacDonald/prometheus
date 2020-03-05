30c90f905911a409638674dd4e27cd974ab684bc
Glitch (buttery-waxflower) <none>
2020-03-05 17:19:20 +0000

    🌂🐗 Checkpoint ./bots/contraction_junction.py:9665611/2169

--- bots/contraction_junction.py
+++ bots/contraction_junction.py
@@ -9,12 +9,21 @@ class ContractionJunction(Bot):
   _frequency = .05
   
   _inner = {
-    "of the": "",
-    "you all": "",
-    "not": "",
-    "over": "",
-    "ever": "",
-    "never": ""
+    "of the": "o'",
+    "you all": "y'all",
+    "not": "'nt",
+    "over": "o'er",
+    "ever": "e'er",
+    "never": "ne'er"
+  }
+  
+  _inner_upper = {
+    "Of the": "O'",
+    "You all": "Y'all",
+    "Not": "'Nt",
+    "Over": "O'er",
+    "Ever": "E'er",
+    "Never": "Ne'er"
   }
   
   _before = {
@@ -44,9 +53,6 @@ class ContractionJunction(Bot):
     "You": "Y",
   }
   
-  _next={"it":"'t","of":"o'","of the":"o'","you":"y'"}
-  _full={"I am":"I'm","let us":"let's"}
-  message=""
   @classmethod
   def description(cls):
     return "`contraction_junction` has a {}% chance to turn a message to contractions".format(cls._frequency * 100)
@@ -54,21 +60,21 @@ class ContractionJunction(Bot):
   def run(self):
     if self._event.isFromABot() or not self._event.isAMessage() or not self._event.isFromChaosUser() or not self._randomUtil.rollDice(self._frequency):
       return
-    # if self._event.isFromABot() or not self._event.isAMessage() or not self._event.isInChannel("Megamoji"):
-    #   return
     
     message = self._event.text()
+    
     for i in self._before:
-      message = re.sub(f'(\\s{i}\\b)|(\\b{i}\\b)', f'\'{self._before[i]}', message, flags=re.IGNORECASE)
-    for i in 
-    for i in self._next:
-      message = message.lower().replace(f" {i} ", f" {self._next[i]}")
-    for i in self._full:
-      message = message.lower().replace(i, self._full[i])
-      
-    print(message)
+      message = re.sub(f'(\\s{i}\\b)|(\\b{i}\\b)', f'\'{self._before[i]}', message, flags = re.IGNORECASE)
+    for i in self._after_upper:
+      message = re.sub(f'(\\b{i}\\s\')|(\\b{i}\')|(\\b{i}\\b)', f'{self._after_upper[i]}\'', message)
+    for i in self._after:
+      message = re.sub(f'(\\b{i}\\s\')|(\\b{i}\')|(\\b{i}\\b)', f'{self._after[i]}\'', message, flags = re.IGNORECASE)
+    for i in self._inner_upper:
+      message = re.sub(f'(\\b{i}\\b)', f'{self._inner_upper[i]}', message)
+    for i in self._inner:
+      message = re.sub(f'(\\b{i}\\b)', f'{self._inner[i]}', message, flags = re.IGNORECASE)
 
-    if message == self._event.text().lower():
+    if message == self._event.text():
       return
     
     identity = IDENTITIES[self._event.user()]
