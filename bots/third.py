5614f5ee70d49810fbbe78edf16ec7328a1f02ab
Glitch (buttery-waxflower) <none>
2020-03-08 03:13:01 +0000

    🚀🍼 Checkpoint ./bots/third.py:9665611/379

--- bots/third.py
+++ bots/third.py
@@ -17,26 +17,18 @@ class Third(Bot):
   def description(cls):
     return "`Third` Makes {} talk in third person".format(cls._target)
   
-  def contains_first(self):
-    container = False
-    if " i " in self._event.text().lower():
-      container=True
-    if "i " in self._event.text().lower():
-      container=True
-    if "i'm" in self._event.text().lower():
-      container=True
-    return container
-  
   def run(self):
     if (
         self._event.isAMessage() 
         and self._event.isFrom(self._target) 
         and self._randomUtil.rollDice(self._frequency) 
-        and self.contains_first()
         and not self._event.isFromABot()
     ):  
       text = self._event.text()
-      text = re.sub(f'\\bi\\b', f' {self._target} ', text, flags = re.IGNORECASE)
-      text = re.sub(f'\\bi\'m\\b', f' {self._target} is ', text, flags = re.IGNORECASE)
+      text = re.sub(f'\\bi\\b', f'{self._target}', text, flags = re.IGNORECASE)
+      text = re.sub(f'\\bi\'m\\b', f'{self._target} is', text, flags = re.IGNORECASE)
+      
+      if text == self._event.text():
+        return
       
       self._postUtil.replacePost(self._event, text)
