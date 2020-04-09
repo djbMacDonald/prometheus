73b5285e767c633c4df4ae8a90ffd6dac31efbb9
Glitch (buttery-waxflower) <none>
2020-04-09 15:29:39 +0000

    🎤😹 Checkpoint ./bots/lex.py:9665611/2969 ./constant/people.py:9665611/8

--- bots/lex.py
+++ bots/lex.py
@@ -1,9 +1,10 @@
 import random
+import re
 from bots._bot import Bot
 
 class Lex(Bot):
   
-  _active = False
+  _active = True
   _frequency = .01
   
   @classmethod
@@ -11,23 +12,32 @@ class Lex(Bot):
     return "Scrambles letters in some words."
   
   def run(self):
+    print(1)
     if (
         self._event.isFromABot() 
-        or not not self._event.text()
+        or not self._event.text()
         or not self._event.isAMessage() 
         or not self._event.isFromChaosUser() 
         or not self._randomUtil.rollDice(self._frequency)
     ):
       return
+    print(2)
     
-    words = map(self._doStuff, self._event.text().split(' '))
-    newMessage = "".join(words)
-    if self._event.text() == newMessage:
+    words = re.findall(r'\w+', self._event.text())
+    longWord = max(words, key=len)
+    scramble = self._scrambleWord(longWord)
+    newString = self._event.text().replace(longWord, scramble)
+    
+    if self._event.text() == newString:
       return;
-    self._postUtil.replacePost(newMessage)
+    self._postUtil.replacePost(newString)
   
-  def doStuff(self, word):
+  def _scrambleWord(self, word):
     if len(word) < 4:
       return word
+    
     first, mid, last = word[0], word[1:-1], word[-1]
-    return first + shuffle_string(mid) + last
+    chars = list(mid)
+    random.shuffle(chars)
+    newMid = ''.join(chars)
+    return first + newMid + last
