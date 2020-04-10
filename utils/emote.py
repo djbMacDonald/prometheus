b0f50711de6ebfc3a2aeb54fb640923c640f121e
Glitch (buttery-waxflower) <none>
2020-04-10 16:21:28 +0000

    🚈🏋 Checkpoint ./bots/mock.py:9665611/78 ./utils/emote.py:9665611/844

--- utils/emote.py
+++ utils/emote.py
@@ -1,3 +1,5 @@
+import os
+
 class Emote:
   
   def __init__(self, emotes):
@@ -5,3 +7,24 @@ class Emote:
     
   def getAll(self):
     return self._emotes
+  
+  def getReactionsOnPost(self, event):
+    postData = {
+      'token': os.environ.get('SECRET'),
+      'channel': self._event.channel(),
+      'latest': self._event.id(),
+      'inclusive': True,
+      'oldest': self._event.id()
+    }
+#     req = requests.get('https://slack.com/api/channels.history?{}'.format(urllib.parse.urlencode(postData)))
+    
+#     message = req.json().get('messages')
+#     if not message or len(message) < 1:
+#       return
+#     message = message[0]
+#     if message.get('replies') or message.get('thread_ts'):
+#       return
+#     if not message.get('reactions'):
+#       return
+    
+#     reactionNames = map(lambda reaction: reaction.get('name'), message.get('reactions'))
