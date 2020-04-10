741dd46ea6c5110e33c00f0f49a5e4fb61a65f21
Glitch (buttery-waxflower) <none>
2020-04-10 17:07:15 +0000

    🛋🎒 Checkpoint ./bots/scrambler.py:9665611/109

--- bots/scrambler.py
+++ bots/scrambler.py
@@ -8,17 +8,18 @@ class Scrambler(Bot):
   _active = True
   _frequency = 4
   _target = 'Drew' 
+  _minWords = 4
   
   @classmethod
   def description(cls):
-    return "Has a {}%/(word count^2) chance to scramble {}'s message".format(cls._frequency * 100, cls._target)
+    return "Has a {}%/(word count^2) chance to scramble {}'s message. Ony words if message has {} or more words".format(cls._frequency * 100, cls._target, cls._minWords)
     
   def run(self):
     if self._event.isFromABot() or not self._event.isAMessage() or not self._event.isFrom(self._target):
       return
     words = self._event.text().split(' ')
     word_count=len(words)
-    if  word_count > 3 and self._randomUtil.rollDice(self._frequency / word_count ** 2):
+    if word_count >= self._minWords and self._randomUtil.rollDice(self._frequency / word_count ** 2):
       random.shuffle(words)
       self._postUtil.replacePost(' '.join(words))
       return
