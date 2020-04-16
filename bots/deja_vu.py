cd497075fc340780147947e99917ead4fb8647aa
Glitch (buttery-waxflower) <none>
2020-04-16 17:12:52 +0000

    🍦👡 Checkpoint ./bots/deja_vu.py:9665611/13

--- bots/deja_vu.py
+++ bots/deja_vu.py
@@ -17,6 +17,7 @@ class DejaVu(Bot):
     lines = open('{}_logfile.txt'.format(self._event.channel()), 'r').read().splitlines()
     self._addReactionToMessage('dejavu')
     myline = random.choice(lines)
+    newId = 
     self._addMessage(myline, identity = self._identityUtil.getRandomIdentity())
     #Drew says don't clear the file. What a Jerk
     #self.logToNewFile()
