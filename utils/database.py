74fb242af3fb18b511034a5f6befa9745d476fea
Glitch (buttery-waxflower) <none>
2020-04-20 00:56:31 +0000

    👍💑 Checkpoint ./utils/database.py:14060967/4524

--- utils/database.py
+++ utils/database.py
@@ -1,6 +1,23 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy import Table, Column, Integer, String, MetaData
 
 import hashlib as hl
 
-class 
+class Database:
+  DB_ENGINE = {
+    SQLITE: 'sqlite:///{DB}'
+  }
+  
+  db_engine = None
+  def __init__(self, dbtype, username='', password='', dbname=''):
+    dbtype = dbtype.lower()
+    if dbtype in self.DB_ENGINE.keys():
+      engine_url = self.DB_ENGINE[dbtype].format(DB=dbname)
+      self.db_engine = create_engine(engine_url)
+      print(self.db_engine)
+    else:
+      print("DBType is not found in DB_ENGINE")
+      
+  def initialize_civ_db(self):
+    metadata = Metadata()
+    payload_hashes = Table(Payloads, metadata, Column('PayloadHash', Binary, primary_key=True))
