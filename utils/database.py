db13d6cbe81f4d56282d3eee21771dfa1eec02ac
Glitch (buttery-waxflower) <none>
2020-04-20 01:20:27 +0000

    🖼🍏 Checkpoint ./utils/database.py:14060967/612

--- utils/database.py
+++ utils/database.py
@@ -25,3 +25,17 @@ class Database:
     except Exception as e:
       print("Error occurred during table creation.")
       print(e)
+      
+  def execute_query(self, query=''):
+    if query == '': return
+    with self.db_engine.connect() as connection:
+      try:
+        return connection.execute(query)
+      except Exception as e:
+        print(e)
+  
+  def does_table_exist(self, table_name = ''):
+    if table_name = '': return False
+    sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='{}';"
+    if self.execute_query(sql.format(table_name)).count() == 0:
+      
