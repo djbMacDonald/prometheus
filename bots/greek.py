b53d741819e48a365adc1ca354fea47117cda1a1
Glitch (buttery-waxflower) <none>
2020-04-21 18:05:37 +0000

    💾🛍 Checkpoint ./requirements.txt:15082594/12 ./bots/greek.py:15082594/453

--- bots/greek.py
+++ bots/greek.py
@@ -1,49 +1,14 @@
-import random
-from model.identity import Identity
-from constant.people import USERS
 from bots._bot import Bot
+from googletrans import Translator
 
 class Greek(Bot):
   
-  _active = False
   _frequency = .01
-  _target = 'Brenden'
-  
-  _stuff = {
-    'a': 'α',
-    'b': 'β',
-    'g': 'γ',
-    'd': 'δ',
-    'e': 'ε',
-    'z': 'ζ',
-    'h': 'η',
-    'th': 'θ',
-    'i': 'ι',
-    'k': 'κ',
-    'l': 'λ',
-    'm': 'μ',
-    'n': 'ν',
-    'x': 'χ',
-    'o': 'ω',
-    'p': 'π',
-    'r': 'ρ',
-    's': '',
-    't': '',
-    'u': '',
-    'f': '',
-    'ch': '',
-    'ps': '',
-  }
-  
-  @classmethod
-  def description(cls):
-    return ""
-    # return "Has a {}% chance to replace {}'s spaces with dashes".format(cls._frequency * 100, cls._target)
   
   def run(self):
-    if not self._chaosUserSendsMessage() or not not self._randomUtil.rollDice(self._frequency):
+    if not self._chaosUserSendsMessage() or not self._randomUtil.rollDice(self._frenquency):
       return
-    # if ' ' in self._event.text():
-    #   message = self._event.text().replace(' ', '-')
-    #   self._replacePost(message)
-    #   return
+    
+    translator = Translator()
+    thing = translator.translate(self._event.text(), dest='el')
+    print(thing)
