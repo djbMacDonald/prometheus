35189a23e88d590c63de34d3d42e09d61237eb27
Glitch (buttery-waxflower) <none>
2020-04-21 18:29:35 +0000

    🎻💆 Checkpoint ./bots/greek.py:15082594/1224 ./constant/people.py:15082594/8

--- bots/greek.py
+++ bots/greek.py
@@ -1,19 +1,26 @@
 from bots._bot import Bot
 from googletrans import Translator
+import re
 
 class Greek(Bot):
   
   _active = True
   _frequency = .01
   
+  @classmethod
+  def description(cls):
+    return "Has a {}% chance to replace aa word in the post with Greek, via Google Translate.".format(cls._frequency * 100)
+  
   def run(self):
-    if not self._event.isInChannel('Megamoji') or not self._event.text() or not self._event.isAMessage():
+    if not self._chaosUserSendsMessage() or not self._randomUtil.rollDice(self._frenquency):
       return
     
-    # if not self._chaosUserSendsMessage() or not self._randomUtil.rollDice(self._frenquency):
-    #   return
-    
     translator = Translator()
-    thing = translator.translate(self._event.text(), dest='el')
-    print(thing)
-    print(thing.get('text'))
+    
+    words = re.findall(r'\w+', self._event.text())
+    longWord = max(words, key=len)
+    translation = translator.translate(longWord, dest='el').text
+    if translation == longWord:
+      return
+    newString = self._event.text().replace(longWord, "{} [[{}]]".format(translation, longWord))
+    self._replacePost(newString)
