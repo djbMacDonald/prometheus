716f5ce6f77cc4e2ed989e7cd014b724131089dc
Glitch (buttery-waxflower) <none>
2020-04-21 19:05:35 +0000

    🗾📯 Checkpoint ./bots/mock.py:15082594/19

--- bots/mock.py
+++ bots/mock.py
@@ -14,7 +14,7 @@ class Mock(Bot):
       return
     
     messageInfo = self._emoteUtil.getReactionsOnPost(self._event)
-    if not messageInfo.get('reactionNames'):
+    if not messageInfo or not messageInfo.get('reactionNames'):
       return
     intersection = set(messageInfo.get('reactionNames')).intersection(['spongebob-mock', 'mocking-spongebob'])
     if len(intersection) == 0 or messageInfo.get('hasAThread'):
