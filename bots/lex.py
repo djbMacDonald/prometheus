106f6ad759a1e7d85ebe6e16933f22d5589eb016
Glitch (buttery-waxflower) <none>
2020-04-22 18:09:48 +0000

    🏟📠 Checkpoint ./bots/lex.py:15082594/457

--- bots/lex.py
+++ bots/lex.py
@@ -24,17 +24,19 @@ class Lex(Bot):
     words = re.findall(r'\w+', self._event.text())
     longWord = max(words, key=len)
     scramble = self._scrambleWord(longWord)
-    newString = self._event.text().replace(longWord, scramble)
-    
-    if self._event.text() == newString:
+    if longWord == scramble:
       return;
+    
+    newString = self._event.text().replace(longWord, scramble)
     self._replacePost(newString)
   
   def _scrambleWord(self, word):
     if len(word) < 4:
       return word
-    
-    first, mid, last = word[0], word[1:-1], word[-1]
+    if len(word) > 5:
+      first, mid, last = word[:2], word[2:-2], word[-2:]
+    else:
+      first, mid, last = word[:1], word[1:-1], word[-1:]
     chars = list(mid)
     random.shuffle(chars)
     newMid = ''.join(chars)
