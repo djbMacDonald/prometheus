39d836fcb8d29d497444a219bca612fbe661bb3e
Glitch (buttery-waxflower) <none>
2020-05-18 16:07:59 +0000

    🌠😇 Checkpoint ./bots/moogle.py:15082594/323 ./constant/people.py:15082594/2

--- bots/moogle.py
+++ bots/moogle.py
@@ -1,10 +1,11 @@
 from bots._bot import Bot
+import re
 
 class Moogle(Bot):
   
-  _active = False
-  _target = 'Victoria'
-  _frequency = 0
+  _active = True
+  _target = 'Doug'
+  _frequency = 1
   
   _pronouns = ['you', 'me', 'i', 'they', 'them', 'him', 'her', 'he', 'she', 'it', 'we', 'me', 'us']
   
@@ -16,5 +17,9 @@ class Moogle(Bot):
     if not self._targetSendsMessageToChannel():
       return
     newMessage = self._event.text()
-    words = newMessage.split(' ');
+    for pronoun in self._pronouns:
+      newMessage = re.sub(r'\b{}\b'.format(pronoun), 'TEST', newMessage, flags=(re.MULTILINE | re.IGNORECASE))
+    if self._event.text() == newMessage:
+      return;
+    self._replacePost(newMessage)
     
